d58c0e27bc568a8a86fe232867504613c7c618d5
Callum McIntyre <callum.mcintyre@citrix.com>
2018-03-01 17:44:46 +0000

    Add an inital setup.py for testing
    Signed-off-by: Callum McIntyre <callum.mcintyre@citrix.com>

---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'ragelib'
+DESCRIPTION = 'A helper library for RAGE'
+URL = 'https://code.citrite.net/users/callumiandam/repos/ragelib' # TODO: move to github!
+EMAIL = 'callum.mcintyre@citrix.com'
+AUTHOR = 'Callum McIntyre'
+REQUIRES_PYTHON = '>=3.6.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'selenium', 'bs4', 'tqdm'
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+# TODO: Need to add a README! Use it for long_description
+#with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = '\n' + f.read()
+long_description = "TODO: The README for ragelib"
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    use_scm_version=True,
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license=None, # TODO: Check license (BSD like RAGE?)
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
+)
